5d582ea224dc9bb9fc455ac947abd2fdb9e07cc6
OKaluzhyn <o.kaluzhyn@gmail.com>
2016-07-18 13:01:14 +0300

    Создали первый тест

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Square.java
@@ -0,0 +1,18 @@
+package ru.stqa.pft.sandbox;
+
+/**
+ * Created by o.kaluzhin on 12.07.2016.
+ */
+public class Square {
+
+  public double l;
+
+  public Square(double l){
+    this.l = l;
+  }
+
+  public double area(){
+    return  this.l * this.l;
+  }
+
+}
