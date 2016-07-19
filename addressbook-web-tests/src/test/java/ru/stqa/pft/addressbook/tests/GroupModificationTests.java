654c914d13553d0b3a0fa1c2a2246520f4a82899
OKaluzhyn <o.kaluzhyn@gmail.com>
2016-07-19 13:59:02 +0300

    вообще трешак

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -0,0 +1,20 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.GroupData;
+
+/**
+ * Created by o.kaluzhin on 19.07.2016.
+ */
+public class GroupModificationTests extends TestBase {
+
+  @Test
+  public void testGroupModification(){
+    app.getNavigationHelper().goToGroupPage();
+    app.getGroupHelper().selectGroup();
+    app.getGroupHelper().initGroupModification();
+    app.getGroupHelper().fillGroupForm(new GroupData("test1", "test2", "test3"));
+    app.getGroupHelper().sumbitGroupModificatiom();
+    app.getGroupHelper().returnGroupPage();
+  }
+}
